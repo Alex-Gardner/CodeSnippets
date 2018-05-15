f67ad687a9aff200c48325df21b18a252a310518
Alex Gardner <a.gardner.g@gmail.com>
2018-05-14 21:53:46 -0400

    secret keys .gitignored

---
+++ Code Snippets/http requests/Twitter Libs/cred.js
@@ -0,0 +1,9 @@
+const cred = {
+  consumer_key:         'CK1GW2OCJtxQgVkMx2AWbRkRI',
+  consumer_secret:      'CTUmTC6ta3Rsglq8itt0pJTWAtjtEqghvgkpBt2d1hs0ap8q57',
+  access_token:         '846415922172755969-uSnOBnfNsNU23ytTnyzv7Tmk4lk6w6I',
+  access_token_secret:  'e3ekmV25hQkN8t29hgFgMVtUy14xxSW21MqQng3qyo2wM',
+  timeout_ms:           30*1000,  // optional HTTP request timeout to apply to all requests.
+}
+
+export cred;
--- Code Snippets/http requests/Twitter Libs/twit-index.js
+++ Code Snippets/http requests/Twitter Libs/twit-index.js
@@ -1,12 +1,7 @@
 var Twit = require("twit")
+import cred from './cred'
 
-var T = new Twit({
-  consumer_key:         'CK1GW2OCJtxQgVkMx2AWbRkRI',
-  consumer_secret:      'CTUmTC6ta3Rsglq8itt0pJTWAtjtEqghvgkpBt2d1hs0ap8q57',
-  access_token:         '846415922172755969-uSnOBnfNsNU23ytTnyzv7Tmk4lk6w6I',
-  access_token_secret:  'e3ekmV25hQkN8t29hgFgMVtUy14xxSW21MqQng3qyo2wM',
-  timeout_ms:           30*1000,  // optional HTTP request timeout to apply to all requests.
-})
+var T = new Twit(cred)
 
 T.get('search/tweets', { q: 'sushi since:2017-02-11', count: 20 }, function(err, data, response) {
   console.log(data)
